632fe7937d91a27d903cd5c59d3221084e21841f
Catarina F. M. Castro <catfmcastro@gmail.com>
2024-08-18 18:01:32 -0300

    reading graph file OK

--- Ex01/App.java
+++ Ex01/App.java
@@ -13,44 +13,48 @@ import java.util.Scanner;
 
 public class App {
 
-  private Graph graph;
-  private String fileName = ""; // file name for test graph
-
-  public static boolean isOptionValid(int selected) {
-    boolean awnser;
-    if (selected > -1 && selected <= 1) {
-      awnser = true;
-    } else {
-      System.out.println("Opção selecionada inválida! Tente novamente");
-      awnser = false;
+    public static boolean isOptionValid(int selected) {
+        boolean awnser;
+        if (selected > -1 && selected <= 1) {
+            awnser = true;
+        } else {
+            System.out.println("Opção selecionada inválida! Tente novamente");
+            awnser = false;
+        }
+        return awnser;
     }
-    return awnser;
-  }
 
-  public static void main(String[] args) {
-    // Atributes
-    Scanner sc = new Scanner(System.in);
-    int selected = 0;
-    boolean exit = false;
+    public static void main(String[] args) {
+        // Atributes
+        Scanner sc = new Scanner(System.in);
+        int selected = 0;
+        boolean exit = false;
 
-    while (!exit) {
-      System.out.println("\n\nOlá! Seja bem-vindo. -------------------\nO que deseja fazer?");
-      System.out.println("[ 0 ]: Sair");
-      System.out.println("[ 1 ]: Representação de Grafo");
+        Graph graph;
+        String fileName = ""; // file name for test graph
 
-      System.out.print("\nDigite a opção que deseja: ");
-      selected = Integer.parseInt(sc.nextLine());
+        while (!exit) {
+            System.out.println("\n\nOlá! Seja bem-vindo. -------------------\nO que deseja fazer?");
+            System.out.println("[ 0 ]: Sair");
+            System.out.println("[ 1 ]: Representação de Grafo");
 
-      switch (selected) {
-        case 0:
-          exit = true;
-          System.out.println("Obrigada! Saindo...");
-          sc.close();
-          break;
-        case 1:
-          System.out.println("\nRepresentação de Grafo Selecionada.");
-          break;
-      }
+            System.out.print("\nDigite a opção que deseja: ");
+            selected = Integer.parseInt(sc.nextLine());
+
+            switch (selected) {
+                case 0:
+                    exit = true;
+                    System.out.println("Obrigada! Saindo...");
+                    sc.close();
+                    break;
+                case 1:
+                    System.out.println("\nRepresentação de Grafo Selecionada.");
+                    System.out.print("\nQual o nome do arquivo que contém o grafo?: ");
+                    fileName = sc.nextLine();
+
+                    graph = new Graph(fileName);
+                    break;
+            }
+        }
     }
-  }
 }
--- Ex01/Graph.java
+++ Ex01/Graph.java
@@ -1,18 +1,73 @@
 /*
- * Graph representation using adjacancy list
- * Representação de um grafo utilizando lista de adjacência 
-*/
+ * Graph representation using adjacency list
+ * Representação de um grafo utilizando lista de adjacência
+ */
 
 package Ex01;
 
-import java.io.RandomAccessFile;
+import java.io.File;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.Scanner;
 
 public class Graph {
+
     // Atributes
-    private int vertexes;
-    private int edges;
-    protected int predecessors[];
-    protected int successors[];
+    private int n;
+    private int m;
+
+    protected int origin[];
+    protected int destiny[];
+
+    protected int forwardStar[];
+    protected int reverseStar[];
+
+    private Scanner file;
+
+    // Constructor
+    public Graph(String fileName) {
+        int pos = 1; // begin position with 1, so it aligns with vertexes number
+
+        try {
+            // open graph .txt file
+            String dir = "./Ex01/Tests/" + fileName + ".txt";
+            this.file = new Scanner(new File(dir));
+
+            // vertexes number and edges number
+            this.n = file.nextInt();
+            this.m = file.nextInt();
+
+            // origin and destiny vertexes of each edge
+            this.origin = new int[m + 1]; 
+            this.destiny = new int[m + 1];
+
+            // forward and reverse star arrays
+            this.forwardStar = new int[m + 2];
+            this.reverseStar = new int[m + 2];
+
+            while(file.hasNextInt()) {
+                origin[pos] = file.nextInt();
+                destiny[pos] = file.nextInt();
+                pos++;
+            }
+
+            // indicates end of list
+            this.forwardStar[n+1] = m + 1;
+            this.reverseStar[n+1] = m + 1;
+            
+        } catch (Exception e) {
+            System.out.println();
+            System.err.println(e);
+            e.printStackTrace();
+        }
+    }
+
+    // Getters & Setters
+    public int GetN() {
+        return n;
+    }
+
+    public int GetM() {
+        return m;
+    }
 }
