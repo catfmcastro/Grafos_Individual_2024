1c020c1072eb5d2745a3b92ed14b8f7d68c72bda
Catarina F. M. Castro <catfmcastro@gmail.com>
2024-08-18 14:13:08 -0300

    fix menu

--- Ex01/App.java
+++ Ex01/App.java
@@ -1,16 +1,56 @@
-/* 
-* Implementação 01 - Representação de Grafos
-* Implementation 01 - Graph Representation
-*
-* @catfmcastro Catarina F. M. Castro
-*
-* Teoria dos Grafos e Computabilidade - PUC Minas
-*/ 
+/*
+ * Implementação 01 - Representação de Grafos
+ * Implementation 01 - Graph Representation
+ *
+ * @catfmcastro Catarina F. M. Castro
+ *
+ * Teoria dos Grafos e Computabilidade - PUC Minas
+ */
 
 package Ex01;
 
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) {
-        
+
+  private Graph graph;
+  private String fileName = ""; // file name for test graph
+
+  public static boolean isOptionValid(int selected) {
+    boolean awnser;
+    if (selected > -1 && selected <= 1) {
+      awnser = true;
+    } else {
+      System.out.println("Opção selecionada inválida! Tente novamente");
+      awnser = false;
     }
+    return awnser;
+  }
+
+  public static void main(String[] args) {
+    // Atributes
+    Scanner sc = new Scanner(System.in);
+    int selected = 0;
+    boolean exit = false;
+
+    while (!exit) {
+      System.out.println("\n\nOlá! Seja bem-vindo. -------------------\nO que deseja fazer?");
+      System.out.println("[ 0 ]: Sair");
+      System.out.println("[ 1 ]: Representação de Grafo");
+
+      System.out.print("\nDigite a opção que deseja: ");
+      selected = Integer.parseInt(sc.nextLine());
+
+      switch (selected) {
+        case 0:
+          exit = true;
+          System.out.println("Obrigada! Saindo...");
+          sc.close();
+          break;
+        case 1:
+          System.out.println("\nRepresentação de Grafo Selecionada.");
+          break;
+      }
+    }
+  }
 }
--- Ex01/Graph.java
+++ Ex01/Graph.java
@@ -1,6 +1,6 @@
 /*
- * Graph representation using adjacancy list - 
- * Representação de um grafo utilizando lista de adjacência - 
+ * Graph representation using adjacancy list
+ * Representação de um grafo utilizando lista de adjacência 
 */
 
 package Ex01;
@@ -13,4 +13,6 @@ public class Graph {
     // Atributes
     private int vertexes;
     private int edges;
+    protected int predecessors[];
+    protected int successors[];
 }
